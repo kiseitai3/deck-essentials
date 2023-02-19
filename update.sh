938778bb1c7e32d0251b3b40a40a5eb048c0034c
Luis Miguel Santos <48329768+kiseitai3@users.noreply.github.com>
2023-02-18 20:16:28 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+if [[ $(id -u) != 0 ]]; then
+  pkexec "$0"
+  exit 0
+fi
 
+pushd /home/deck/
+
+./.deck-essentials/uninstall.sh
+
+git clone 
+
+mv deck-essentials .deck-essentials
+
+./.deck-essentials/install.sh
+
+popd
