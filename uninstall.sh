9144610a6361869f64cb765fe326d42d7011d4e1
Luis Miguel Santos <48329768+kiseitai3@users.noreply.github.com>
2023-02-18 20:13:24 -0500

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ $(id -u) != 0 ]]; then
+  pkexec "$0"
+  exit 0
+fi
+
+systemctl daemon-reload
+systemctl stop deck-essentials.service
+systemctl disable deck-essentials.service
+systemctl daemon-reload
+
+rm /etc/systemd/system/deck-essentials.service
+
+pushd /home/deck/
+rm -r .deck-essentials
+
+popd
