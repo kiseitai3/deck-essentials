878de832eb165cb92562a5d1bc51e970a16e560f
Luis Miguel Santos <48329768+kiseitai3@users.noreply.github.com>
2023-02-20 16:15:28 -0500

    Update deck-essentials.sh

--- deck-essentials.sh
+++ deck-essentials.sh
@@ -32,8 +32,8 @@ install_config () {
       cp "$1" "$TARGET"
     ;;
     aggregate-nic-config)
-      python3 deck-essentials-aggregate-nics.py down
-      python3 deck-essentials-aggregate-nics.py up
+      #python3 deck-essentials-aggregate-nics.py down
+      #python3 deck-essentials-aggregate-nics.py up
     ;;
   esac
 }
