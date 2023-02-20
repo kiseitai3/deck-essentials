d8e25c1b18aba913a55ed795962b1a94f0f0aa46
Luis Miguel Santos <48329768+kiseitai3@users.noreply.github.com>
2023-02-20 10:42:03 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 if [[ $(id -u) != 0 ]]; then
-  export USERHOME="$HOME"
   pkexec "$0"
   exit 0
 fi
 
-pushd $HOME/
-
-echo "User home: $USERHOME"
-echo "Using home: $HOME"
+pushd /home/deck/
 
 ./.deck-essentials/uninstall.sh
 
